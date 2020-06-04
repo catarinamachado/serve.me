308feaf2d336ba801253064d1f376b77b403d1c1
Tiago Fontes <tiagorafaelfontes39@gmail.com>
2020-06-04 15:42:30 +0100

    Base backend

---
+++ Backend/ServeMe/src/main/java/EA_Team/ServeMe/ServeMeApplication.java
@@ -0,0 +1,13 @@
+package EA_Team.ServeMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeMeApplication.class, args);
+	}
+
+}
