d98de134f7dff52307d1d7914bb3ee3e32811f78
catarinamachado <catarinamachado11@gmail.com>
2020-06-11 01:47:35 +0100

    Add general css to external file

--- Frontend/serve.me/src/App.vue
+++ Frontend/serve.me/src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import './assets/styles/serve-me.css';
 import TheNavbar from './components/TheNavbar.vue'
 import LandingPage from './components/LandingPage.vue'
 import TheFooter from './components/TheFooter.vue'
@@ -20,14 +21,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
