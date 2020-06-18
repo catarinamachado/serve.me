981e4ef476e69a100f469945991ade4eb98ca240
catarinamachado <catarinamachado11@gmail.com>
2020-06-19 00:08:06 +0100

    Change title

--- Frontend/serve.me/src/App.vue
+++ Frontend/serve.me/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <vue-headful title="Serve.Me"/>
     <TheNavbar/>
     <router-view></router-view>
     <TheFooter/>
--- Frontend/serve.me/src/main.js
+++ Frontend/serve.me/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import vueHeadful from 'vue-headful'
 import App from './App.vue'
 import router from "./router";
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
+
+Vue.component('vue-headful', vueHeadful);
 
 new Vue({
   router,
