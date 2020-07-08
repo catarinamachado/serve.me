58ab815b8e3ef31fbd9779f36fb279727d05fcf2
catarinamachado <catarinamachado11@gmail.com>
2020-07-08 21:58:36 +0100

    Add global variable URL

--- Frontend/serve.me/src/main.js
+++ Frontend/serve.me/src/main.js
@@ -17,6 +17,8 @@ Vue.config.productionTip = false
 
 Vue.component('vue-headful', vueHeadful);
 
+Vue.prototype.$backend = 'http://25.113.37.183:8083/api';
+
 new Vue({
   router,
   render: h => h(App),
