7291032798a0728c2f5468ec68a6df7b114ecc75
Tiago Fontes <tiagorafaelfontes39@gmail.com>
2020-07-11 23:45:21 +0100

    Add Crypt | Minor fix on Login Alert

--- Frontend/serve.me/src/axios.js
+++ Frontend/serve.me/src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = 'http://25.113.37.183:8083/api'
+//const API_URL = 'http://25.113.37.183:8083/api'
+const API_URL = 'http://localhost:8083/api'
 
 export default axios.create({
   baseURL: API_URL,
--- Frontend/serve.me/src/store/consts.js
+++ Frontend/serve.me/src/store/consts.js
@@ -1,4 +1,5 @@
-const URL = "http://25.113.37.183:8083/api"
+//const URL = "http://25.113.37.183:8083/api"
+const URL = "http://localhost:8083/api"
 
 export default {
     URL: URL
